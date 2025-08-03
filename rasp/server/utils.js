2d0f8389f259a8fa6f150e66b5674435d9f9ca0c
Anmol Jhamb <talktoanmol@outlook.com>
2025-08-03 07:29:01 +0530

    null_check added

--- rasp/server/utils.js
+++ rasp/server/utils.js
@@ -3,32 +3,34 @@ const fs = require("fs");
 const path = require("path");
 
 function saveToFile(values) {
-    const time = moment().format("h:mm:ss a, DD/MM/YY");
+  const time = moment().format("h:mm:ss a, DD/MM/YY");
 
-    values = { ...values, time };
+  values = { ...values, time };
 
-    let pastData = fs.readFileSync(path.join(__dirname, "data.json"), {
-        encoding: "utf-8",
+  let pastData = [];
+
+  try {
+    pastData = fs.readFileSync(path.join(__dirname, "data.json"), {
+      encoding: "utf-8",
     });
+    pastData = JSON.parse(pastData) ?? [];
+  } catch (error) {
+    console.log(error);
+  }
 
-    pastData = JSON.parse(pastData);
-
-    let newData = [...pastData, values];
-    fs.writeFileSync(
-        path.join(__dirname, "data.json"),
-        JSON.stringify(newData)
-    );
+  let newData = [...pastData, values];
+  fs.writeFileSync(path.join(__dirname, "data.json"), JSON.stringify(newData));
 }
 
 function getData() {
-    return JSON.parse(
-        fs.readFileSync(path.join(__dirname, "data.json"), {
-            encoding: "utf-8",
-        })
-    );
+  return JSON.parse(
+    fs.readFileSync(path.join(__dirname, "data.json"), {
+      encoding: "utf-8",
+    }),
+  );
 }
 
 module.exports = {
-    saveToFile,
-    getData,
+  saveToFile,
+  getData,
 };
